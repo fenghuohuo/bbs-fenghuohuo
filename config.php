cff985b9c68449f63c91dadd508c39a8aeccba0a
root <446100702@qq.com>
2017-03-20 11:04:00 +0800

    提交

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'bbs',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => 'https://fenghuohuo.win',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
